0073df7b6679412a8402ee12b290e41748ea2994
Adam Bergman <adam@fransvilhelm.com>
2016-09-07 23:47:14 +0200

    Removed file

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import rawly from '../lib';
-
-describe('rawly', () => {
-  it('should have unit test!', () => {
-    expect(false, 'we expected this package author to add actual unit tests.');
-  });
-});
